2dd3652fb9a6d567d0a93836d6ee18571edf0eb8
joyosphere <joyosphere@gmail.com>
2024-10-25 01:09:18 +0900

    feat: front static data 분리

--- app/page.tsx
+++ app/page.tsx
@@ -1,83 +1,13 @@
 import { CardList } from "@/components/player/CardList";
-
-const cardsData = [
-  {
-    id: "1",
-    profileImage: "/images/jm_avatar.png",
-    description: "첫 번째 아티스트의 앨범",
-    songs: [
-      {
-        title: "노래 1",
-        audioSrc:
-          "https://joyo-musics.s3.ap-northeast-2.amazonaws.com/random-acoustic-electronic-guitar-136427.mp3",
-      },
-      {
-        title: "노래 2",
-        audioSrc:
-          "https://joyo-musics.s3.ap-northeast-2.amazonaws.com/background-ambient-music-in-lydian-mode-21059.mp3",
-      },
-      {
-        title: "노래 3",
-        audioSrc:
-          "https://joyo-musics.s3.ap-northeast-2.amazonaws.com/morax-unlocked-hacker-mode-142916.mp3",
-      },
-    ],
-  },
-  // 이지훈님
-  {
-    id: "2",
-    profileImage: "/images/jh_avatar.png",
-    description: "두 번째 아티스트의 앨범",
-    songs: [
-      {
-        title: "지훈님 노래 A",
-        audioSrc: "/audio/jh_song1.mp3",
-      },
-      {
-        title: "지훈님 노래 B",
-        audioSrc: "/audio/jh_song2.mp3",
-      },
-    ],
-  },
-  // 서영학님
-  {
-    id: "3",
-    profileImage: "/images/yh_avatar.png",
-    description: "세 번째 아티스트의 앨범",
-    songs: [
-      {
-        title: "영학님 노래 A", // 수정
-        audioSrc: "/audio/yh_song1.mp3",
-      },
-      {
-        title: "영학님 노래 B", // 수정
-        audioSrc: "/audio/yh_song2.mp3",
-      },
-    ],
-  },
-  // 김재영님
-  {
-    id: "4",
-    profileImage: "/images/jy_avatar.png",
-    description: "네 번째 아티스트의 앨범",
-    songs: [
-      {
-        title: "재영님 노래 A",
-        audioSrc: "/audio/jm_song1.mp3",
-      },
-      {
-        title: "재영님 노래 B",
-        audioSrc: "/audio/jm_song2.mp3",
-      },
-    ],
-  },
-];
+import { songList } from "@/data/songList";
 
 function Home() {
+  const TITLE = "13조 KT 엠티뮤직 뽕짜라뽕 👏";
+
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">13조 엠티 뮤직 👏</h1>
-      <CardList cards={cardsData} />
+      <h1 className="text-2xl font-bold mb-4">{TITLE}</h1>
+      <CardList cards={songList} />
     </div>
   );
 }
---
+++ data/songList.ts
@@ -0,0 +1,84 @@
+export const songList = [
+  {
+    id: "1",
+    profileImage: "/images/jm_avatar.png",
+    description: "첫 번째 아티스트의 앨범",
+    songs: [
+      {
+        title: "노래 1",
+        audioSrc:
+          "https://joyo-musics.s3.ap-northeast-2.amazonaws.com/random-acoustic-electronic-guitar-136427.mp3",
+      },
+      {
+        title: "노래 2",
+        audioSrc:
+          "https://joyo-musics.s3.ap-northeast-2.amazonaws.com/background-ambient-music-in-lydian-mode-21059.mp3",
+      },
+      {
+        title: "노래 3",
+        audioSrc:
+          "https://joyo-musics.s3.ap-northeast-2.amazonaws.com/morax-unlocked-hacker-mode-142916.mp3",
+      },
+    ],
+  },
+  // 이지훈님
+  {
+    id: "2",
+    profileImage: "/images/jh_avatar.png",
+    description: "두 번째 아티스트의 앨범",
+    songs: [
+      {
+        title: "지훈님 노래 A",
+        audioSrc: "/audio/jh_song1.mp3",
+      },
+      {
+        title: "지훈님 노래 B",
+        audioSrc: "/audio/jh_song2.mp3",
+      },
+      {
+        title: "지훈님 노래 C",
+        audioSrc: "/audio/jh_song3.mp3",
+      },
+    ],
+  },
+  // 서영학님
+  {
+    id: "3",
+    profileImage: "/images/yh_avatar.png",
+    description: "세 번째 아티스트의 앨범",
+    songs: [
+      {
+        title: "영학님 노래 A",
+        audioSrc: "/audio/yh_song1.mp3",
+      },
+      {
+        title: "영학님 노래 B",
+        audioSrc: "/audio/yh_song2.mp3",
+      },
+      {
+        title: "영학님 노래 C",
+        audioSrc: "/audio/yh_song3.mp3",
+      },
+    ],
+  },
+  // 김재영님
+  {
+    id: "4",
+    profileImage: "/images/jy_avatar.png",
+    description: "네 번째 아티스트의 앨범",
+    songs: [
+      {
+        title: "재영님 노래 A",
+        audioSrc: "/audio/jm_song1.mp3",
+      },
+      {
+        title: "재영님 노래 B",
+        audioSrc: "/audio/jm_song2.mp3",
+      },
+      {
+        title: "재영님 노래 C",
+        audioSrc: "/audio/jm_song3.mp3",
+      },
+    ],
+  },
+];
