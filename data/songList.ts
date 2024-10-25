aa08f90374c19c467e41e310c79d6ca25fff7217
JIHOON LEE <jihoon7705@gmail.com>
2024-10-25 16:55:08 +0900

    feat : 이지훈 음악, 사진 변경

--- data/songList.ts
+++ data/songList.ts
@@ -24,20 +24,26 @@ export const songList = [
   // 이지훈님
   {
     id: "2",
-    profileImage: "/images/jh_avatar.png",
-    description: "두 번째 아티스트의 앨범",
+    profileImage: "/images/jh_avatar.jpeg",
+    description: "기분에 따라 듣는 노래가 알록달록합니다. 오늘은 신이 나 🥳",
     songs: [
       {
-        title: "지훈님 노래 A",
-        audioSrc: "/audio/jh_song1.mp3",
+        title: "강타 - 그 해 여름",
+        audioSrc:
+          "https://rr3---sn-3pm7kn7r.googlevideo.com/videoplayback?expire=1729862588&ei=XEcbZ--XKbea7OsPydLA6QY&ip=59.1.15.114&id=o-AIACneLhRVq9EWfFrsRDIIQwoXZI-3cA3KvizUGgvZNc&itag=140&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&gcr=kr&bui=AQn3pFQgMYNCFFZXb1_6FB03K8kSML3sOSWvgiqM0yYFA8hLF69XMzIkF6U329RapyvoDwuLzfmBHrpK&spc=qtApAYrzwuwC-xHHs-pC0S7sG1tMVIYR7LVkrw6_sRjk1WrMIGZ1Pl8u-Ear&vprv=1&svpuc=1&mime=audio/mp4&ns=OsN5KEqYvmYM7cosctTFIDEQ&rqh=1&gir=yes&clen=4581480&dur=282.959&lmt=1614145226026332&keepalive=yes&fexp=24350590,24350655,24350675,24350705,24350730,24350737,24350739,51307727,51312688,51326932&c=MWEB&sefc=1&txp=2311222&n=ZrdHvNF3gzHVIA&sparams=expire,ei,ip,id,itag,source,requiressl,xpc,gcr,bui,spc,vprv,svpuc,mime,ns,rqh,gir,clen,dur,lmt&sig=AJfQdSswRgIhAIx0_gyq-e1SLZEkwGN5UoMCu_ZndBt096aSgWO5tgddAiEA5RDEGYwsHhI2V0flGA-fG7oDkkU9vZf1L9V0A4LTARk%3D&title=%EA%B7%B8+%ED%95%B4+%EC%97%AC%EB%A6%84+Late+Summer+Night&rm=sn-3u-bh2ss7z,sn-3pmz67s&rrc=79,104&req_id=77090fd8efada3ee&rms=nxu,au&redirect_counter=2&cms_redirect=yes&cmsv=e&ipbypass=yes&met=1729841021,&mh=Ga&mip=218.232.83.19&mm=30&mn=sn-3pm7kn7r&ms=nxu&mv=u&mvi=3&pl=24&lsparams=ipbypass,met,mh,mip,mm,mn,ms,mv,mvi,pl,rms&lsig=ACJ0pHgwRAIgeinrJKuVk9Tj7IOzdVqKDDtRXt1aFGSu879BLm9fIp0CIDRlaknu1XDsx1_XDQ_nfI16LE7qZlUQKHisOuLR3x1o",
       },
       {
-        title: "지훈님 노래 B",
-        audioSrc: "/audio/jh_song2.mp3",
+        title: "Adam Levine - No One Else Like You",
+        audioSrc:
+          "https://rr1---sn-n3cgv5qc5oq-bh2l6.googlevideo.com/videoplayback?expire=1729862812&ei=PEgbZ9zzAYHn6dsPudnImQc&ip=84.119.247.54&id=o-AE1fLeCyZEcrT396xN-6dxkk4qqyJInsgI62li6fL5rS&itag=140&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&rms=au,au&bui=AQn3pFS7--pd0HOORZ9V0n1sWLOgc82x8sFKw8xOfIYUnX7a-ZqGMunwud2xdGLv_Oy_8jVN47YEdb26&spc=qtApAf9Klss6zmVfYWnqY-xUAbeIKULIx3C_yEUkQmzcdyXsn708GJZ4Ur7x&vprv=1&svpuc=1&mime=audio/mp4&ns=1XpfZOmryPHbZGft9UzMVC8Q&rqh=1&gir=yes&clen=3384651&dur=209.095&lmt=1629376818297384&keepalive=yes&fexp=24350591,24350655,24350675,24350705,24350729,24350737,24350739,51312688,51326931&c=MWEB&sefc=1&txp=5311224&n=Ud2osN8BT25V1g&sparams=expire,ei,ip,id,itag,source,requiressl,xpc,bui,spc,vprv,svpuc,mime,ns,rqh,gir,clen,dur,lmt&sig=AJfQdSswRQIgK_FkoAeSYKGiWE_WS7NrsCu8NlO4XfPO3iA5uJbTzj0CIQD5CniIsmmA5RwotqSd38jA6L99VCBZ9Xhh_bd5tMjJNA%3D%3D&title=%5B%EB%B9%84%EA%B8%B4+%EC%96%B4%EA%B2%8C%EC%9D%B8+OST%5D+%EB%84%88+%EA%B0%99%EC%9D%80+%EC%82%AC%EB%9E%8C%EC%9D%B4+%ED%95%84%EC%9A%94%ED%95%B4+%7C+Adam+Levine+-+No+One+Else+Like+You+%5B%EA%B0%80%EC%82%AC%ED%95%B4%EC%84%9D/%EC%9E%90%EB%A7%89/Lyrics%5D&redirect_counter=1&rm=sn-4g5erl7z&rrc=104&req_id=9866ea5bb5f5a3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&met=1729841229,&mh=r-&mip=218.232.83.19&mm=31&mn=sn-n3cgv5qc5oq-bh2l6&ms=au&mv=u&mvi=1&pl=20&lsparams=ipbypass,met,mh,mip,mm,mn,ms,mv,mvi,pl,rms&lsig=ACJ0pHgwRQIgcyu-Qaxd1NvTfHcNyHrTbBOyLKVtLxRIgLE2kC42RHICIQDt2-GRsOaiNw2K7KRl-uA-iQZHJbsnhH6y-lPQKdCveQ%3D%3D",
       },
       {
-        title: "지훈님 노래 C",
-        audioSrc: "/audio/jh_song3.mp3",
+        title: "리쌍 - TV를 껐네",
+        audioSrc: "/audio/leessang.mp3",
+      },
+      {
+        title: "실리카겔 (Silica Gel) - NO PAIN",
+        audioSrc: "/audio/noPain.mp3",
       },
     ],
   },
